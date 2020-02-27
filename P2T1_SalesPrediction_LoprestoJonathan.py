9bbc2acc738e6dff70f84fe9140ce68bc48f63d0
Jtxl97 <61558321+Jtxl97@users.noreply.github.com>
2020-02-27 13:10:31 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_LoprestoJonathan.py
@@ -0,0 +1,13 @@
+# This program will calculate projected profit
+# 2/20/2020
+# CTI-110 P2T1 - Sales Prediction
+# Jonathan Lopresto
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit
+print('The profit is $', format(profit, ',.2f'))
