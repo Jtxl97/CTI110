acbae91dc01afc1c270029fc606d80df6267c00e
Jtxl97 <61558321+Jtxl97@users.noreply.github.com>
2020-03-24 23:04:16 -0400

    Add files via upload

---
+++ P3LAB_Debugging_JonathanLopresto.py
@@ -0,0 +1,24 @@
+# CTI-110
+# P3LAB - Debugging
+# Jonathan Lopresto
+# 3/24/2020
+
+#When score is equal to A_score display "Your grade is A."
+#When score is equal to B_score display "Your grade is B."
+#When score is equal to C_score display "Your grade is C."
+#When score is equal to D_score display "Your grade is D."
+#When score is not equal to any of the above display "Your grade is F."
+
+if score == A_score:
+    print('Your grade is A.')
+    else:
+        if score == B_score:
+            print('Your grade is B.')
+            else:
+                if score == C_score:
+                    print('Your grade is C.')
+                    else:
+                        if score == D_score:
+                            print('Your grade is D.')
+                            else:
+                                print('Your grade is F.')
